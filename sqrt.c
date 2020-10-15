7af06d8c479bcc4b55746e07101a1389c722af21
Saransh <b18191@students.iitmandi.ac.in>
2020-10-15 15:40:14 +0530

    check for input validity

--- sqrt.c
+++ sqrt.c
@@ -13,6 +13,11 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
+	if (input==0)
+	{
+		printf("invalid input\n");
+		return 0;
+	}
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting");
 	return(0);
